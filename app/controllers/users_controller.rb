baf42449a4608fe70fbe74353e9c1fd71794f617
Phuong Doan <phuongdoan@Phuongs-MacBook-Pro.local>
2016-01-13 02:55:51 +0700

    migrate user using devise

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -69,6 +69,6 @@ class UsersController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
-      params.require(:user).permit(:name, :email, :password, :is_admin, :is_organization, :avatar_url, :description, :contact_address, :contact_city)
+      params.require(:user).permit(:name, :is_admin, :is_organization, :avatar_url, :description, :contact_address, :contact_city)
     end
 end
