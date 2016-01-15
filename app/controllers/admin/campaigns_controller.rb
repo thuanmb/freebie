5a54601fb6d6f6a87943bc5a0c6e6f8d54993183
An Tran Minh <tranminhan.my@gmail.com>
2016-01-15 22:11:40 +0700

    clean admin layout

--- app/controllers/admin/campaigns_controller.rb
+++ app/controllers/admin/campaigns_controller.rb
@@ -11,4 +11,19 @@ class Admin::CampaignsController < AdminController
     @campaign = Campaign.find(params[:id])
   end
 
+  def update
+    @campaign = Campaign.find(params[:id])
+    if @campaign.update(campaign_params)
+      flash[:success] = "Update '#{@campaign.title}' successfully"
+      redirect_to admin_campaign_path(@campaign)
+    else       
+      render 'admin/campaigns/edit'
+    end 
+  end
+
+  private 
+
+  def campaign_params
+    params.require(:campaign).permit(:title, :description)
+  end 
 end
