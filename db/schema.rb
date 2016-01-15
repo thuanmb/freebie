1515d88b554e4da7c4183a8e4ab6614d162a6894
Phuong Doan <phuongdoan@phuongdoan.local>
2016-01-15 23:33:55 +0700

    migrate schema

--- db/schema.rb
+++ db/schema.rb
@@ -110,9 +110,12 @@ ActiveRecord::Schema.define(version: 20160115153804) do
     t.string   "title"
     t.text     "description"
     t.string   "location"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-    t.string   "image"
+    t.datetime "created_at",         null: false
+    t.datetime "updated_at",         null: false
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
