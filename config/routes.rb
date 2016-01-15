82c95eb33b469a7478628c4ccfa9a5885f2831d0
Phuong Doan <phuongdoan@phuongdoan.local>
2016-01-15 23:30:31 +0700

    move to trash, restore

--- config/routes.rb
+++ config/routes.rb
@@ -16,7 +16,12 @@ Rails.application.routes.draw do
   resources :conversations, only: [:index, :show, :destroy] do
     member do
       post :reply
+      post :restore
     end
+    collection do
+      delete :empty_trash
+    end
+
   end
   resources :messages, only: [:new, :create]
 
