b4c1f6ad3bf513e86382fb35fc376f50c6100050
An Tran Minh <tranminhan.my@gmail.com>
2016-01-19 15:18:45 +0700

    add main image attachment to post

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160115153804) do
+ActiveRecord::Schema.define(version: 20160119081302) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -110,12 +110,13 @@ ActiveRecord::Schema.define(version: 20160115153804) do
     t.string   "title"
     t.text     "description"
     t.string   "location"
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
-    t.string   "image_file_name"
-    t.string   "image_content_type"
-    t.integer  "image_file_size"
-    t.datetime "image_updated_at"
+    t.datetime "created_at",              null: false
+    t.datetime "updated_at",              null: false
+    t.string   "image"
+    t.string   "main_image_file_name"
+    t.string   "main_image_content_type"
+    t.integer  "main_image_file_size"
+    t.datetime "main_image_updated_at"
   end
 
   create_table "users", force: :cascade do |t|
