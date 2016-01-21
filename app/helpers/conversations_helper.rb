f0dcbaa13cc260fe49249bb2017a55129f594dfb
Phuong Doan <phuongdoan@phuongdoan.local>
2016-01-21 18:10:20 +0700

    fix for message layout

--- app/helpers/conversations_helper.rb
+++ app/helpers/conversations_helper.rb
@@ -1,8 +1,8 @@
 module ConversationsHelper
-  def mailbox_section(title, current_box, params)
+  def mailbox_section(title, type, current_box, params)
     link_class = 'nav-link'
-    link_class += ' active' if title.downcase == current_box
-    url_params = {box: title.downcase}
+    link_class += ' active' if type.downcase == current_box
+    url_params = {box: type}
     url_params = url_params.merge(conversation: params[:conversation]) if params[:conversation]
     content_tag :li, link_to(title.capitalize, me_conversations_path(url_params), class: link_class), class: 'nav-item'
   end
