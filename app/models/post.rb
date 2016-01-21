64247a62c22bf279ee59a7fc93b0ef590b8a48b8
Thuan Bui <thubui@atlassian.com>
2016-01-21 16:31:27 +0700

    precompile assets and revert post.rb

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,11 +2,8 @@ class Post < ActiveRecord::Base
   belongs_to :user
   has_many :post_items
 
-  has_attached_file :main_image,
-  								styles: {medium: "300x300>", thumb: "100x100>"},
-                  :path => ":rails_root/app/assets/images/posts/:id/:style_:basename.:extension",
-                  :url => "/app/assets/images/posts/:id/:style_:basename.:extension"
-  validates_attachment_content_type :main_image, :content_type => ["image/jpg", "image/jpeg", "image/png", "image/gif"]
+  has_attached_file :main_image, styles: {medium: "300x300>", thumb: "100x100>"}
+  validates_attachment_content_type :main_image, content_type: /\Aimage\/.*\Z/
 
   def main_image_url
     image_url.present? ? image_url : main_image.url
