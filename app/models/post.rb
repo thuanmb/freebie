079306ce1dc10a7eb4cea648f8cfcc78ef5f37e8
An Tran Minh <tranminhan.my@gmail.com>
2016-01-22 14:46:55 +0700

    add status to post

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,20 +1,37 @@
 class Post < ActiveRecord::Base
   belongs_to :user
   has_many :post_items
+  validates :status, inclusion: { in: %w(drafted published closed) }
 
   has_attached_file :main_image, styles: {medium: "300x300>", thumb: "100x100>"}
   validates_attachment_content_type :main_image, content_type: /\Aimage\/.*\Z/
 
+  def self.published_posts
+    Post.where('status != ?', 'published')
+  end
+
   def main_image_url
-    image_url.present? ? image_url : main_image.url
+    self.image_url.present? ? self.image_url : self.main_image.url
   end 
 
-  def public?
-    status == 'public'
+  def published?
+    self.status == 'published'
   end
 
   def closed?
-    status == 'closed'
+    self.status == 'closed'
+  end
+
+  def drafted?
+    self.status == 'drafted'
+  end
+
+  def publish
+    self.status = 'published' unless self.status == 'published'
+  end
+
+  def close
+    self.status = 'closed' unless self.status == 'closed'
   end
 
 end
