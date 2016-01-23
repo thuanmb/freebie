217d8b3fe69de8ec88b97c031d3fc124612d79a7
Phuong Doan <phuongdoan@phuongdoan.local>
2016-01-24 00:16:54 +0700

    remove js for header and alpha style

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,38 +24,4 @@
 //= require_tree .
 //= require_tree ../../../vendor/assets/javascripts/.
 
-var CAROUSEL_AMIN_CLASS = "carousel-anim";
-
-$(document).ready(function () {
-  var secondaryNav = $('.navbar-header');
-  if (secondaryNav.length > 0) {
-    var secondaryNavTopPosition = secondaryNav.offset().top;
-
-    $(window).on('scroll', function(){;
-
-      if($(window).scrollTop() > secondaryNavTopPosition ) {
-        $("body").addClass('is-fixed');
-        setTimeout(function() {
-          secondaryNav.addClass('animate-children');
-        }, 50);
-      } else {
-        $("body").removeClass('is-fixed');
-        setTimeout(function() {
-          secondaryNav.removeClass('animate-children');
-        }, 50);
-      }
-    });
-
-    $(".cd-secondary-nav-trigger").click(function () {
-      secondaryNav.toggleClass("is-visible");
-    });
-
-    $('#carousel-campaign-list').on('slide.bs.carousel', function (e) {
-      $(".carousel-item").removeClass(CAROUSEL_AMIN_CLASS)
-      $(e.relatedTarget).addClass(CAROUSEL_AMIN_CLASS)
-    });
-
-    $('[data-toggle="popover"]').popover();
-  }
-});
 
