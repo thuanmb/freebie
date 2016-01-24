dd196bbb9a83d0e1a1a2910ddd3adfec430afc20
Thuan Bui <thubui@atlassian.com>
2016-01-24 09:51:40 +0700

    Revert the fixed menu

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,4 +24,30 @@
 //= require_tree .
 //= require_tree ../../../vendor/assets/javascripts/.
 
+$(document).ready(function () {
+    var secondaryNav = $('.navbar-header');
+    if (secondaryNav.length > 0) {
+        var secondaryNavTopPosition = secondaryNav.offset().top;
+
+        $(window).on('scroll', function(){
+
+            if($(window).scrollTop() > secondaryNavTopPosition ) {
+                $("body").addClass('is-fixed');
+                setTimeout(function() {
+                    secondaryNav.addClass('animate-children');
+                }, 50);
+            } else {
+                $("body").removeClass('is-fixed');
+                setTimeout(function() {
+                    secondaryNav.removeClass('animate-children');
+                }, 50);
+            }
+        });
+
+        $(".cd-secondary-nav-trigger").click(function () {
+            secondaryNav.toggleClass("is-visible");
+        });
+    }
+});
+
 
