24f758e7384c166f90a283e2ef044e768835e0f3
An Tran Minh <tranminhan.my@gmail.com>
2016-01-28 18:07:18 +0700

    fix bug

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,6 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy, :publish, :close, :reopen]
-  before_action :authenticate_user!, except: [:index, :show, :search]
+  before_action :authenticate_user!, except: [:index, :show, :search, :byLocation]
 
   # GET /posts
   # GET /posts.json
